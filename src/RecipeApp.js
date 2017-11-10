7a2968d40aceea61a1a068f5fd0775f4518a2b42
Andrej <strudl.ruzca@gmail.com>
2017-11-10 10:53:20 +0000

    refactored to state from default props

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -5,11 +5,40 @@ import RecipeList from './RecipeList'
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: "Avocado with eggs",
+          instructions: "Cut bread. Bake eggs. Combine all ingredients. Put some salt and olive oil.",
+          ingredients: ["bread", "avocado", "eggs","salt","olive oil"],
+          img: "avocadoegs.jpeg"
+        },
+        {
+          id: 2,
+          title: "Real american burger",
+          instructions: "Bake meat just at the right temperature. Add ingredients together",
+          ingredients: ["200g meat", "bread","ketchup","salad","tomato"],
+          img: "burger.jpeg"
+       },
+        {
+          id: 3,
+          title: "Wraps",
+          instructions: "Bake ingredients and wrap them in tortilla.",
+          ingredients: ["4 tortilla","1 chicken breast", "all sorts of different vegetable", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "wraps.jpeg"
+        }
+        ],
+        nextRecipeId: 3,
+    }
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeList recipes = {this.state.recipes} />
       </div>
     );
   }
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,35 +4,13 @@ import PropTypes from 'prop-types';
 import './RecipeList.css'
 
 class RecipeList extends Component {
-    static defaultProps = {
-    recipes: [
-      {
-        title: "Avocado with eggs",
-        instructions: "Cut bread. Bake eggs. Combine all ingredients. Put some salt and olive oil.",
-        ingredients: ["bread", "avocado", "eggs","salt","olive oil"],
-        img: "avocadoegs.jpeg"
-      },
-      {
-        title: "Real american burger",
-        instructions: "Bake meat just at the right temperature. Add ingredients together",
-        ingredients: ["200g meat", "bread","ketchup","salad","tomato"],
-        img: "burger.jpeg"
-      },
-      {
-        title: "Wraps",
-        instructions: "Bake ingredients and wrap them in tortilla.",
-        ingredients: ["4 tortilla","1 chicken breast", "all sorts of different vegetable", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
-        img: "wraps.jpeg"
-      }
-    ]
-  }
   
   static propTypes = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired
   }
     render(){
      const recipes = this.props.recipes.map((r,index) => (
-      <Recipe key={index} {...r} />
+      <Recipe key={r.id} {...r} />
     ));
     
     return (
